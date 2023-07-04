a89354cb06c7c0d65d398f800cc92f95a4f43078
tristanotaku <tristanotaku@gmail.com>
2023-07-04 20:57:02 +0700

    Tristan Firdaus Update CRUD for Table Pasien

---
+++ CRUD/pasien/delete.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $conn = mysqli_connect('localhost', 'root', '', 'rumahsakit');
+
+    $query = "DELETE FROM pasien WHERE id = $id";
+
+    if (mysqli_query($conn, $query)) {
+        header('Location: index.php?message=success');
+    } else {
+        header('Location: index.php?message=error');
+    }
+
+    mysqli_close($conn);
+}
+?>
