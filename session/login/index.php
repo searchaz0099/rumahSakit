ab39797ae9e05ea1d773f18e387df044fd22ff9f
searchaz0099 <searchaz0099@gmail.com>
2023-07-07 14:19:12 +0700

    1

--- session/login/index.php
+++ session/login/index.php
@@ -8,12 +8,13 @@
                     <input class="input" type="text" placeholder="email" name="email">
                     <input class="input" type="password" placeholder="passsword" name="password">
                     <input class="login" type="submit" name="login" placeholder="Login">
+                    <p class="message">Already registered? <a href="#">Sign In</a></p>
                 </form>
                 <form class="register-form" method="post" action="index.php">
                     <input class="input" type="text" placeholder="email" name='email'>
                     <input class="input" type="password" placeholder="passsword" name='password'>
                     <input class="input" type="text" placeholder="nama" name='nama'>
-                    <input class="create" type="submit" name="create" placeholder="Create">
+                    <input class="login" type="submit" name="create" placeholder="Create">
                     <p class="message">Already registered? <a href="#">Sign In</a></p>
                 </form>
             </div>
@@ -28,9 +29,9 @@
 
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $submit = $_POST['login'];
+    $login = $_POST['login'];
 
-    if ($submit){
+    if ($login){
         $sql = "SELECT * FROM user WHERE email = '$email' and password = '$password'";
         $query = mysqli_query($conn, $sql);
         $row = mysqli_fetch_array($query);
@@ -67,5 +68,33 @@
 ?>
 
 <?php
+    error_reporting(E_ALL & E_NOTICE & E_DEPRECATED);
+    $conn = mysqli_connect('localhost', 'root', '', 'rumahSakit');
+
+    $email = $_POST['email'];   
+    $password = $_POST['password'];
+    $nama = $_POST['nama'];
+    $create = $_POST['create'];
+
     
+
+    if($create){
+
+        $sql = "INSERT INTO user VALUES ('$email', '$password', '$nama', 'member')";
+        if($email == ""){
+            echo '<email tidak boleh kosong';
+        }if($password == ""){
+            echo 'password tidak boleh kosong';
+        }if($nama == ""){
+            echo 'nama tidak boleh kosong';
+        }elseif($email != ""){
+            mysqli_query($conn, $sql);
+            echo "
+            <script>
+                    alert('data berhasil dimasukkan');
+                    document.location.href='index.php';
+                </script>";
+        }
+    }
+
 ?>
