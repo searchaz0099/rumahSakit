1611f82eb23e61809282b8cbccb5e768b5540069
searchaz0099 <searchaz0099@gmail.com>
2023-07-07 15:30:27 +0700

    v

--- CRUD/pasien/delete.php
+++ CRUD/pasien/delete.php
@@ -2,9 +2,10 @@
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
+
     $conn = mysqli_connect('localhost', 'root', '', 'rumahsakit');
 
-    $query = "DELETE FROM pasien WHERE id = $id";
+    $query = "DELETE FROM pasien WHERE id_dokter = $id";
 
     if (mysqli_query($conn, $query)) {
         header('Location: index.php?message=success');
